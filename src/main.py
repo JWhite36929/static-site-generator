78a105d12d2c56190f4a56a78b91dfda54046363
JWhite36929 <jamestwhite361@gmail.com>
2025-08-27 00:35:31 -0400

    finalize TextNode with validation

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("hello world")
+from textnode import TextNode 
+from textnode import TextType
+
+def main():
+    example1 = TextNode("Hello", TextType.BOLD, "https://omarchy.org")
+    example2 = TextNode("oops", TextType.ITALIC)
+
+    print(example2)
+    print(example1)
+
+
+if __name__ == "__main__":
+    main()
