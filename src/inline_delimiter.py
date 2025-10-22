c48bf894335ebb99053c390e9486f762c7143399
JWhite36929 <jamestwhite361@gmail.com>
2025-10-22 14:05:00 -0400

    plain text to list of nodes

--- src/inline_delimiter.py
+++ src/inline_delimiter.py
@@ -150,3 +150,31 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
+def text_to_textnodes(text):
+    """
+    This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)
+
+    becomes
+
+    [
+    TextNode("This is ", TextType.TEXT),
+    TextNode("text", TextType.BOLD),
+    TextNode(" with an ", TextType.TEXT),
+    TextNode("italic", TextType.ITALIC),
+    TextNode(" word and a ", TextType.TEXT),
+    TextNode("code block", TextType.CODE),
+    TextNode(" and an ", TextType.TEXT),
+    TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+    TextNode(" and a ", TextType.TEXT),
+    TextNode("link", TextType.LINK, "https://boot.dev"),
+    ]
+
+    """
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_inline_delimiter.py
+++ src/test_inline_delimiter.py
@@ -5,6 +5,7 @@ from inline_delimiter import(
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 from textnode import TextNode, TextType
@@ -165,6 +166,72 @@ class TestInlineDelimiter(unittest.TestCase):
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
+        )
+    
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
+    
+    def test_text_to_textnodes_no_formatting(self):
+        text = "This is a simple text with no formatting."
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is a simple text with no formatting.", TextType.TEXT),
+            ],
+            nodes,
+        )
+    
+    def test_text_to_textnodes_only_code(self):
+        text = "`print('Hello, World!')`"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("print('Hello, World!')", TextType.CODE),
+            ],
+            nodes,
+        )
+    
+    def test_text_to_textnodes_nested(self):
+        text = "This is **bold and _italic_** text"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("bold and _italic_", TextType.BOLD),
+                TextNode(" text", TextType.TEXT),
+            ],
+            nodes,
+        )
+    
+    def test_text_to_textnodes_multiple_images_links(self):
+        text = "Here is an image ![img1](http://img1.com) and a link [link1](http://link1.com) and another image ![img2](http://img2.com)"
+        nodes = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("Here is an image ", TextType.TEXT),
+                TextNode("img1", TextType.IMAGE, "http://img1.com"),
+                TextNode(" and a link ", TextType.TEXT),
+                TextNode("link1", TextType.LINK, "http://link1.com"),
+                TextNode(" and another image ", TextType.TEXT),
+                TextNode("img2", TextType.IMAGE, "http://img2.com"),
+            ],
+            nodes,
         )
 
 if __name__ == "__main__":
