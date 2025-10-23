13054239eda6aa96d65d81cbc8a4de44bdf7dab2
JWhite36929 <jamestwhite361@gmail.com>
2025-10-23 15:30:11 -0400

    block types and block to block type implementation

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import re
 """
 Block markdown will refer to blocks of text separated by newlines
 
@@ -12,6 +14,15 @@ This is a paragraph with **bolded text**
 
 This would be rendered as 3 blocks : a heading block, a paragraph block, and a list block.
 """
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
 
 def markdown_to_blocks(markdown):
     """
@@ -33,3 +44,31 @@ def markdown_to_blocks(markdown):
         blocks.append("\n".join(current_block).strip())
 
     return blocks
+
+def block_to_block_type(block):
+    """
+    Given a block of markdown text, determine its BlockType
+    """
+    heading_pattern = r'^\s*#{1,6}\s+'
+    code_pattern = r'^\s*```'
+    quote_pattern = r'^\s*>\s+'
+    unordered_list_pattern = r'^\s*[-*+]\s+'
+    ordered_list_pattern = r'^\s*\d+\.\s+'
+
+    if re.match(heading_pattern, block):
+        return BlockType.HEADING
+    elif re.match(code_pattern, block):
+        return BlockType.CODE
+    elif re.match(quote_pattern, block):    
+        return BlockType.QUOTE
+    elif re.match(unordered_list_pattern, block):
+        return BlockType.UNORDERED_LIST
+    elif re.match(ordered_list_pattern, block):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
+
+
+
+
+    
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,5 +1,9 @@
 from textnode import TextNode, TextType
-from block_markdown import markdown_to_blocks
+from block_markdown import (
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType,
+)
 import unittest
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -72,3 +76,28 @@ This is another paragraph
             [],
         )
     
+    def test_block_to_block_type(self):
+        self.assertEqual(
+            block_to_block_type("# This is a heading"),
+            BlockType.HEADING,
+        )
+        self.assertEqual(
+            block_to_block_type("```python\nprint('Hello World')\n```"),
+            BlockType.CODE,
+        )
+        self.assertEqual(
+            block_to_block_type("> This is a quote"),
+            BlockType.QUOTE,
+        )
+        self.assertEqual(
+            block_to_block_type("- This is an unordered list item"),
+            BlockType.UNORDERED_LIST,
+        )
+        self.assertEqual(
+            block_to_block_type("1. This is an ordered list item"),
+            BlockType.ORDERED_LIST,
+        )
+        self.assertEqual(
+            block_to_block_type("This is a regular paragraph."),
+            BlockType.PARAGRAPH,
+        )
